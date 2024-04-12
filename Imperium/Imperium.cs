8beed3b2891925e300b597bf8e2276c14563fc3c
giosuel <sirtubelujohnson@gmail.com>
2024-04-12 12:40:27 +0200

    Updated version

--- Imperium/Imperium.cs
+++ Imperium/Imperium.cs
@@ -43,7 +43,7 @@ public class Imperium : BaseUnityPlugin
 {
     public const string PLUGIN_GUID = "giosuel.Imperium";
     public const string PLUGIN_NAME = "Imperium";
-    public const string PLUGIN_VERSION = "0.1.3";
+    public const string PLUGIN_VERSION = "0.1.4";
 
     internal static ManualLogSource Log;
     internal static ConfigFile ConfigFile;
