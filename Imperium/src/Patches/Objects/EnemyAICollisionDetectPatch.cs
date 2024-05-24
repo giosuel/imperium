dc82bcca56292ffb1c9ab785e5943e7304fff535
Digg¢ <70993871+digger1213@users.noreply.github.com>
2024-05-25 08:17:15 +1200

    Fix reporting hits that deal no damage

--- Imperium/src/Patches/Objects/EnemyAICollisionDetectPatch.cs
+++ Imperium/src/Patches/Objects/EnemyAICollisionDetectPatch.cs
@@ -13,12 +13,21 @@ internal class EnemyAICollisionDetectPatch
 {
     [HarmonyPrefix]
     [HarmonyPatch("IHittable.Hit")]
-    private static void HitPatch(EnemyAICollisionDetect __instance, int force, int hitID)
+    private static void HitPatchPrefix(EnemyAICollisionDetect __instance, out bool __state)
     {
+        __state = !__instance.mainScript.isEnemyDead; //pass true to postfix if enemy is currently alive
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch("IHittable.Hit")]
+    private static void HitPatchPostfix(bool __result, bool __state, EnemyAICollisionDetect __instance, int force, int hitID)
+    {
+        if (!__result) return; //if the hit didn't register, don't do anything
+
         var entityName = __instance.mainScript.enemyType.enemyName;
         Imperium.Log.LogInfo(
             $"Entity {entityName} ({__instance.GetInstanceID()}) was hit by {force} damage, ID: {hitID}");
-        if (!__instance.mainScript.isEnemyDead)
+        if (__state) //if the enemy was alive before the hit
         {
             ImpOutput.Send(
                 $"Entity {entityName} was hit by {force} damage.",
